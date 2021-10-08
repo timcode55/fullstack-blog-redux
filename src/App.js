e1fe43da095e4dc66b69c119c77346ebad5a2da4
Tim Petrvalsky <tpet55@yahoo.com>
2021-10-08 09:53:11 -0700

    Finish login functionality and set error displays

--- src/App.js
+++ src/App.js
@@ -1,174 +1,177 @@
-
-
-import React, { useState, useEffect } from 'react';
-import Notification from './components/Notification';
-import blogService from './services/blogs';
-import loginService from './services/login';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import Notification from "./components/Notification";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
+import axios from "axios";
+import "./App.css";
 
 const App = () => {
-	const [ blogs, setBlogs ] = useState([]);
-	const [ logged, setLogged ] = useState(false);
-	const [ errorMessage, setErrorMessage ] = useState(null);
-	const [ updatedList, setUpdatedList ] = useState(false);
+  const [blogs, setBlogs] = useState([]);
+  const [logged, setLogged] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [updatedList, setUpdatedList] = useState(false);
+  const [username, setUsername] = useState("newusertime");
+  const [password, setPassword] = useState("darkness33");
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [url, setUrl] = useState("");
+  const [type, setType] = useState("");
 
+  const [newUser, setNewUser] = useState(null);
 
-	const [ username, setUsername ] = useState('newusertime');
-	const [ password, setPassword ] = useState('darkness33');
-  const [ title, setTitle ] = useState('');
-  const [ author, setAuthor ] = useState('');
-  const [ url, setUrl ] = useState('');
-
-	const [ newUser, setNewUser ] = useState(null);
-
-	useEffect(() => {
+  useEffect(() => {
     setTimeout(() => {
-      
       blogService.getAll().then((initialBlogs) => {
-        console.log(initialBlogs, 'INITIALBLOGS ON UE')
+        console.log(initialBlogs, "INITIALBLOGS ON UE");
         setBlogs(initialBlogs);
       });
     }, 500);
-    
-	}, [updatedList]);
+  }, [updatedList]);
 
   useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    const loggedUserJSON = window.localStorage.getItem("loggedBlogappUser");
     if (loggedUserJSON) {
-      const user = JSON.parse(loggedUserJSON)
-      setNewUser(user)
-      blogService.setToken(user.token)
-      setLogged(true)
+      const user = JSON.parse(loggedUserJSON);
+      setNewUser(user);
+      blogService.setToken(user.token);
+      setLogged(true);
     }
-  }, [])
+  }, []);
 
-	const addBlog = async (event) => {
-		event.preventDefault();
-		console.log('addBlog Triggered');
-		const blogObject = {
-			title: title,
+  const addBlog = async (event) => {
+    event.preventDefault();
+    const blogObject = {
+      title: title,
       author: author,
       url: url
-		};
-    console.log(blogObject, 'blogobject*****')
-    axios.post('http://localhost:3003/api/blogs', {token: newUser.token}, blogObject)
-		blogService.create(blogObject);
-			setBlogs(blogs.concat(blogObject));
-      setUpdatedList(!updatedList)
-			console.log(blogs, 'BLOGS IN CREATE');
-			setTitle('');
-			setAuthor('');
-			setUrl('');
-	
-	};
+    };
+    console.log(blogObject, "blogobject*****");
+    axios.post(
+      "http://localhost:3003/api/blogs",
+      { token: newUser.token },
+      blogObject
+    );
+    blogService.create(blogObject);
+    setBlogs(blogs.concat(blogObject));
+    setUpdatedList(!updatedList);
+    setErrorMessage(`A new blog ${title} by ${author} added`);
+    setType("blog");
+    setTimeout(() => {
+      setErrorMessage(null);
+      setType(null);
+    }, 5000);
+    setTitle("");
+    setAuthor("");
+    setUrl("");
+  };
 
-	const handleLogin = async (event) => {
-		console.log('handlelogin called');
-		event.preventDefault();
-		try {
-			const user = await loginService.login({
-				username,
-				password
-			});
-			console.log(user, 'USER199******');
-      window.localStorage.setItem(
-        'loggedBlogappUser', JSON.stringify(user)
-      ) 
-      setLogged(true)
-			blogService.setToken(user.token);
-			setNewUser(user);
-			setUsername('');
-			setPassword('');
-		} catch (exception) {
-			setErrorMessage('wrong credentials');
-			setTimeout(() => {
-				setErrorMessage(null);
-			}, 5000);
-		}
-	};
+  const handleLogin = async (event) => {
+    console.log("handlelogin called");
+    event.preventDefault();
+    try {
+      const user = await loginService.login({
+        username,
+        password
+      });
+      console.log(user, "USER199******");
+      window.localStorage.setItem("loggedBlogappUser", JSON.stringify(user));
+      setLogged(true);
+      blogService.setToken(user.token);
+      setNewUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      setErrorMessage("wrong credentials");
+      setType("error");
+      setTimeout(() => {
+        setErrorMessage(null);
+        setType(null);
+      }, 5000);
+    }
+  };
 
   const handleLogOut = (e) => {
-    window.localStorage.clear()
-    setLogged(false)
-  }
+    window.localStorage.clear();
+    setLogged(false);
+  };
 
-	const loginForm = () => (
-		<form onSubmit={handleLogin}>
-			<div>
-				username
-				<input
-					type="text"
-					value={username}
-					name="Username"
-					onChange={({ target }) => setUsername(target.value)}
-				/>
-			</div>
-			<div>
-				password
-				<input
-					type="password"
-					value={password}
-					name="Password"
-					onChange={({ target }) => setPassword(target.value)}
-				/>
-			</div>
-			<button type="submit">login</button>
-		</form>
-	);
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  );
 
   const handleTitle = (e) => {
-		setTitle(e.target.value);
-	};
+    setTitle(e.target.value);
+  };
   const handleAuthor = (e) => {
-		setAuthor(e.target.value);
-	};
+    setAuthor(e.target.value);
+  };
   const handleUrl = (e) => {
-		setUrl(e.target.value);
-	};
+    setUrl(e.target.value);
+  };
 
-	const blogForm = () => (
-		<form onSubmit={addBlog}>
+  const blogForm = () => (
+    <form onSubmit={addBlog}>
       <h1>Add a New Blog</h1>
+      title: <input onChange={handleTitle} value={title || ""} />
+      author: <input onChange={handleAuthor} value={author || ""} />
+      url: <input onChange={handleUrl} value={url || ""} />
+      <button type="submit">save</button>
+    </form>
+  );
 
-      title: <input onChange={handleTitle} value={title || ''} />
-      author: <input onChange={handleAuthor} value={author || ''} />
-      url: <input onChange={handleUrl} value={url || ''} />
-			<button type="submit">save</button>
-		</form>
-	);
+  const loggedIn = window.localStorage.getItem("loggedBlogappUser");
 
-  const loggedIn = window.localStorage.getItem('loggedBlogappUser')
- 
-	console.log(newUser, 'NEWUSER');
-	console.log(blogs, 'BLOGS 249');
-	const showBlogs = blogs.filter((blog) => blog.user?.username === newUser?.username);
-	console.log(showBlogs, 'SHOWBLOGS');
-	console.log(loggedIn,  'loggedIn******');
-	console.log(logged,  'logged146****');
-	console.log(updatedList,  'updatedList****');
+  console.log(newUser, "NEWUSER");
+  console.log(blogs, "BLOGS 249");
+  const showBlogs = blogs.filter(
+    (blog) => blog.user?.username === newUser?.username
+  );
+  console.log(showBlogs, "SHOWBLOGS");
+  console.log(loggedIn, "loggedIn******");
+  console.log(logged, "logged146****");
+  console.log(updatedList, "updatedList****");
 
-	return (
-		<div>
-			<h1>Blogs</h1>
-			<Notification message={errorMessage} />
+  return (
+    <div>
+      <h1>Blogs</h1>
+      <Notification message={errorMessage} type={type} />
 
-			{newUser === null || !loggedIn ? (
-				loginForm()
-			) : (
-				<div>
-					<h4>{newUser.name} logged in</h4>
+      {newUser === null || !loggedIn ? (
+        loginForm()
+      ) : (
+        <div>
+          <h4>{newUser.name} logged in</h4>
           {loggedIn && <button onClick={() => handleLogOut()}>Logout </button>}
           {blogForm()}
-					{loggedIn && logged && showBlogs.map((item) => {
-            return <h2 key={item.id}>{item.title}</h2>;
-					})}
-					
-				</div>
-			)}
-
-			
-		</div>
-	);
+          {loggedIn &&
+            logged &&
+            showBlogs.map((item) => {
+              return <h2 key={item.id}>{item.title}</h2>;
+            })}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default App;
--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
-const Notification = ({ message }) => {
-	if (message === null) {
-		return null;
-	}
+const Notification = ({ message, type }) => {
+  if (message === null) {
+    return null;
+  }
 
-	return <div className="error">{message}</div>;
+  return <div className={type}>{message}</div>;
 };
 
 export default Notification;
